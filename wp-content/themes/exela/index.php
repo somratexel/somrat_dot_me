0503d50a380b78c173a87fc5325c1e0be6534e9b
somratexel <somratexelgm@gmail.com>
2015-09-03 00:13:39 +0600

    fixing menu active bug

--- wp-content/themes/exela/index.php
+++ wp-content/themes/exela/index.php
@@ -36,10 +36,10 @@ get_header(); ?>
 						</a>
 					</div>
 					<div class="col-md-4 wow fadeInRight">
-						<h1 class="site-title"><?php bloginfo('name'); ?></h1>
+						<h1 class="site-title"><a href="<?php echo site_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 						<p><?php bloginfo('description'); ?></p>
 					</div>
-					<div class="col-md-6 wow bounce">
+					<div class="col-md-6 wow fadeInDown">
 						<div class="top-search-container">
 							<input placeholder="Search in site" class="top-search" type="text" name="s">
 							<button class="search-btn" type="submit"><i class="fa fa-search">&nbsp;</i></button>
@@ -47,7 +47,7 @@ get_header(); ?>
 						<div class="top-navigation-container">
 							<ul class="top-manu-list">
 								<li class="top-menu-item" ng-repeat="item in topMenu">
-									<a ng-click="set_active($index)"  ng-class="{active: currentLocation.url==item.url}" class="top-menu-link" title="{{item.title}}" ng-href="{{item.url}}">
+									<a ng-class="{active: currentLocation.url==item.url}" class="top-menu-link" title="{{item.title}}" ng-href="{{item.url}}">
 										<i ng-class="item.classes">&nbsp;</i>
 									</a>
 								</li>
